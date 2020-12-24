7d6680a49c4e337b30109fbd3aa6e8d36713ef2f
Matt2844 <matthewford@Matthews-MacBook-Pro.local>
2020-12-24 02:04:46 -0500

    working on search bar

--- portfolio/src/components/SimpleBackdrop.js
+++ portfolio/src/components/SimpleBackdrop.js
@@ -3,6 +3,10 @@ import Backdrop from '@material-ui/core/Backdrop';
 import CircularProgress from '@material-ui/core/CircularProgress';
 import Button from '@material-ui/core/Button';
 import { makeStyles } from '@material-ui/core/styles';
+/* imports for the snackbar */
+import Snackbar from '@material-ui/core/Snackbar';
+import IconButton from '@material-ui/core/IconButton';
+import CloseIcon from '@material-ui/icons/Close';
 
 const useStyles = makeStyles((theme) => ({
   backdrop: {
@@ -19,6 +23,19 @@ const useStyles = makeStyles((theme) => ({
 export default function SimpleBackdrop () {
   const classes = useStyles();
   const [open, setOpen] = React.useState(false);
+  const [msgOpen, setMsgOpen] = React.useState(false);
+
+  /* for the snack bar */
+  const handleClick = () => {
+    setMsgOpen(true);
+  };
+
+  const handleMsgClose = () => {
+
+    setMsgOpen(false);
+  };
+
+  /* for the search button */
   const handleClose = () => {
     setOpen(false);
   };
@@ -27,16 +44,39 @@ export default function SimpleBackdrop () {
     setTimeout(() => {
       handleClose();
     }, 1100)
+    handleClick();
   };
 
   return (
     <div>
-      <Button className={classes.searchButton} variant="outlined" color="primary" onClick={handleToggle}>
-        Search
+      <div>
+        <Button className={classes.searchButton} variant="outlined" color="primary" onClick={handleToggle}>
+          Search
       </Button>
-      <Backdrop className={classes.backdrop} open={open} onClick={handleClose}>
-        <CircularProgress color="inherit" />
-      </Backdrop>
+        <Backdrop className={classes.backdrop} open={open} onClick={handleClose}>
+          <CircularProgress color="inherit" />
+        </Backdrop>
+      </div>
+      <div>
+        <Snackbar
+          anchorOrigin={{
+            vertical: 'bottom',
+            horizontal: 'left',
+          }}
+          open={msgOpen}
+          autoHideDuration={6000}
+          onClose={handleMsgClose}
+          message="Try searching for 'about' or 'contact' "
+          action={
+            <React.Fragment>
+              <Button color="secondary" size="small" onClick={handleMsgClose}>
+                CLOSE
+          </Button>
+
+            </React.Fragment>
+          }
+        />
+      </div>
     </div>
   );
 }
