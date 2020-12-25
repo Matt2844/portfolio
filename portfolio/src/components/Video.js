ea13411d947a08e60e17221bd663d91a54823140
Matt2844 <matt.hd.ford@gmail.com>
2020-12-24 20:55:47 -0500

    video description section complete

--- portfolio/src/components/Video.js
+++ portfolio/src/components/Video.js
@@ -1,7 +1,7 @@
 
 import React, { useState } from 'react';
 import PlayArrowIcon from '@material-ui/icons/PlayArrow';
-import ThumbUpAltIcon from '@material-ui/icons/ThumbUpAlt';
+
 import '../video.css'
 
 
@@ -11,38 +11,110 @@ const projectData = [
     title: 'Burgers-R-Us',
     thumbnail: './images/burgers-r-us-thumb.png',
     description: 'LHL Group Project',
-    link: 'https://www.youtube.com/embed/VUJDlB1THgA'
+    link: 'https://www.youtube.com/embed/VUJDlB1THgA',
+    aboutProject: {
+      title: 'Burgers-R-Us',
+      videoData: '502K views, developed Dec 4, 2020',
+      description: `An online food ordering system for an individual restaurant. Users can register an account, 
+      login, view checkout, and receive personalized messages. Built primarily with Express, NodeJS, SASS, and jQuery. A
+      group project that was part of the Lighthouse Labs curriculum.`,
+      challenges: `Learning how to code on a group project. This mostly meant learning how to properly use version control
+      when working as a team. We got off to a slow start because we had to figure out do tasks like creating and accepting a pull request.
+      On top of that, this was a remote group project.`,
+      positiveOutcomes: `Discovered that group projects can be fun, and they can make you a better developer. You get to learn from 
+      other developers when you see them do something that you did not know how to do. Of course, it was also great to learn how to 
+      use version control while working with others on the same project.`
+    }
   },
   {
     title: 'Webpage Copy',
     thumbnail: './images/macbook-webpage-copy.png',
     description: 'Independent Project',
-    link: "https://www.youtube.com/embed/Sgu0YXcb_eU"
+    link: "https://www.youtube.com/embed/Sgu0YXcb_eU",
+    aboutProject: {
+      title: 'Apple Webpage Copy',
+      videoData: '23K views, developed Sep 15, 2020',
+      description: `A pixel for pixel copy of a webpage from Apple's website. Built using only HTML and CSS.`,
+      challenges: `I could barely write a line of code before this project. This project was the most simple out of all the projects
+      I have done, yet I had to Google something every 5 minutes. I also had no idea how difficult or easy the project would be.
+      I simply had to think "OK, if I can figure out to copy the nav bar, I can probably do everything else."`,
+      positiveOutcomes: `800 lines of CSS later, I gained a solid understanding of the language, in addition to HTML. Knowing that I 
+      could build a webpage while not knowing a thing gave me a lot of confidence.`
+    }
   },
   {
     title: 'Scheduler',
     thumbnail: './images/scheduler-thumb.png',
     description: 'LHL Independent Project',
-    link: 'https://www.youtube.com/embed/jYyT3PK9YZg'
+    link: 'https://www.youtube.com/embed/jYyT3PK9YZg',
+    aboutProject: {
+      title: 'Scheduler',
+      videoData: '57K views, developed Dec 17, 2020',
+      description: `A management system built with ReactJS. Users can schedule an interview,
+        edit an appointment, and delete an appointment. Linked to a database that is updated through
+        API requests. Independent project that was part of the Lighthouse Labs curriculum.`,
+      challenges: `Since this was my first React project, learning how to use React would be the most obvious challenge.
+      Even though I was very comfortable with Javascript, learning React is no small task. It takes HTML, CSS, JS, and
+      packages it all together. At the same time it introduces new concepts. Surprisingly, it did not take very long to wrap my head around the idea of state or props.
+      My biggest challenge was learning JSX syntax.`,
+      positiveOutcomes: `Besides becoming comfortable with React and its constituents like JSX,
+      I learned that coding requires a mental shift at times. In my personal experience, learning React was more about
+      trying to wrap my head around concepts, rather than learning through repetition. For example, writing a lot of jQuery
+      is a great way to improve your jQuery skills. When it comes to improving your React skills, you might have more luck
+      by reading theory or watching Youtube tutorials.
+      Essentially, React taught me that I need to be extremely flexible while learning.`,
+
+    }
   },
   {
     title: 'Tweeter',
     thumbnail: './images/tweeter-thumbnail.png',
     description: 'LHL Independent Project',
-    link: 'https://www.youtube.com/embed/jtYK8moIw34'
+    link: 'https://www.youtube.com/embed/jtYK8moIw34',
+    aboutProject: {
+      title: 'Tweeter',
+      videoData: '19K views, developed Nov 20, 2020',
+      description: `A Twitter clone built using jQuery, HTML, and CSS. An independent project that was part of the Lighthouse Labs curriculum.`,
+      challenges: `Dynamically rendering data and getting that data to persist after refreshing the page. Going into 
+      this project I did not have a good understanding of dynamic data and how it interacts with the server. It was difficult
+      to visualize what is happening behind the scenes.`,
+      positiveOutcomes: `Had the opportunity to realize that dynamic content is not as difficult as it seems. Also, it is a pretty 
+      good feeling when you click refresh and still see the data sitting there.`
+    }
   },
   {
     title: 'MS-Paint',
     thumbnail: './images/ms-paint1.png',
     description: 'Just For Fun',
-    link: 'https://www.youtube.com/embed/wgrhSCet7Jg'
+    link: 'https://www.youtube.com/embed/wgrhSCet7Jg',
+    aboutProject: {
+      title: 'MS-Paint Hat Tip',
+      videoData: '1.7M views, developed Nov 10, 2020',
+      description: `A Microsoft Paint themed project, built for fun during my spare time.`,
+      challenges: `A month before building this brainless app, I came across a web developers portfolio where you could
+      draw a moustache on her face. I thought it was the coolest thing in the world at the time, and I wanted to figure out how it
+      was done. Figuring out how on earth she did that was sadly the biggest challenge.`,
+      positiveOutcomes: `Overall, coding and building web apps is fun. However, there are often deadlines, difficult problems, roadblocks, 
+      and specific requirements that have to be met. Taking a step back after a serious project to build something creative and light is a great way
+      to ensure web development is always fun and balanced.`
+    }
   },
 
   {
     title: 'Coding Notebook',
     thumbnail: './images/pieces-of-code.png',
     description: 'Just For Fun',
-    link: 'https://www.youtube.com/embed/qsCvRFJ1wTE'
+    link: 'https://www.youtube.com/embed/qsCvRFJ1wTE',
+    aboutProject: {
+      title: 'Coding Notebook',
+      videoData: '3K views, developed Nov 5, 2020',
+      description: `A personal notebook to store useful material so that it can be referenced again in the future. Also helps solidify new concepts.
+      Project is meant to be ongoing.`,
+      challenges: `Having the time to update it. It is built so that is is pretty easy to maintain and update, however, when you are learning
+      10 new things a day, being able to update it all the time is difficult.`,
+      positiveOutcomes: `Experiencing the benefits of web development first hand. Web apps are usually intended to make our lives
+      easier, and this project has certainly done that. I can reference the content in this project efficiently. If I put the material into a word document or physical notebook, I would probably forget about it, and it would be difficult to reference. `
+    }
   },
 
 ]
@@ -52,7 +124,11 @@ const projectData = [
 export default function Video (props) {
 
   const [videoSrc, setVideoSrc] = useState(projectData[2].link)
-
+  const [descTitle, setTitle] = useState(projectData[2].aboutProject.title)
+  const [videoData, setVideoData] = useState(projectData[2].aboutProject.videoData)
+  const [desc, setDesc] = useState(projectData[2].aboutProject.description)
+  const [challenges, setChallenges] = useState(projectData[2].aboutProject.challenges)
+  const [outcomes, setOutcomes] = useState(projectData[2].aboutProject.positiveOutcomes)
 
   return (
 
@@ -61,31 +137,29 @@ export default function Video (props) {
       <div className="video-screen-container">
         <iframe width="854" height="480" src={videoSrc} frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
         <div className="description">
-          <h3 className="project-title">Scheduler</h3>
-          <h5 className="project-views-date">57K views, developed Dec 17, 2020 </h5><hr />
+          <h3 className="project-title">{descTitle}</h3>
+          <h5 className="project-views-date">{videoData}</h5><hr />
           <h3 className="project-description-title">Description</h3>
-          <p className="project-description-text">A management system built with ReactJS. Users can schedule an interview,
-          edit an appointment, and delete an appointment. Linked to a database that is updated through
-            API requests. Independent project that was part of the Lighthouse Labs curriculum.</p><hr />
+          <p className="project-description-text">{desc}</p><hr />
           <h3 className="project-challenges-title">Challenges</h3>
-          <p className="project-challenges-text">Since this was my first React project, learning how to use React would be the most obvious challenge.
-          Even though I was very comfortable with Javascript, learning React is no small task. It takes HTML, CSS, JS, and
-          packages it all together. At the same time it introduces new concepts. Surprisingly, it did not take very long to wrap my head around the idea of state or props.
-          My biggest challenge was learning JSX syntax.</p><hr />
+          <p className="project-challenges-text">{challenges}</p><hr />
           <h3 className="project-outcomes-title">Positive Outcomes</h3>
-          <p className="project-outcomes-text">Besides becoming comfortable with React and its constituents like JSX,
-          I learned that coding requires a mental shift at times. In my personal experience, learning React was more about
-          trying to wrap my head around concepts, rather than learning through repetition. For example, writing a lot of jQuery
-          is a great way to improve your jQuery skills. When it comes to improving your React skills, you might have more luck
-          by reading theory or watching Youtube tutorials.<p></p>
-          Essentially, React taught me how to be extremely flexible while learning.</p>
+          <p className="project-outcomes-text">{outcomes}</p>
         </div>
       </div>
 
       <div className="video-container" >
         <div className="card">
           {projectData.map(project => (
-            <div className="video-card" onClick={() => setVideoSrc(project.link)}>
+            <div className="video-card" onClick={() => {
+              setVideoSrc(project.link)
+              setTitle(project.aboutProject.title)
+              setVideoData(project.aboutProject.videoData)
+              setDesc(project.aboutProject.description)
+              setChallenges(project.aboutProject.challenges)
+              setOutcomes(project.aboutProject.positiveOutcomes)
+            }}
+            >
               <img className="video-thumbnail" alt="video thumbnail" src={project.thumbnail} />
               <div className="video-description-container">
                 <h5 className="video-card-title">{project.title}</h5>
